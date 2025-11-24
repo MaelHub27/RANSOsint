3365c21db069628ca71b0178e285e62ed5064659
MaelHub27 <mael.gretener@gmail.com>
2025-11-24 11:42:27 +0100

    ajout dernière API et complétion de la liste des domaines

--- README.md
+++ README.md
@@ -15,6 +15,8 @@ The two files used in the src folder are :
 
 - ransomware.py : the bot who will detect the ransomware attack and notify (or not) a user
 
+The file domains.json is used to add or remove domains to verify, you may add your domain in this file if you want to check for potential ransomware attacks. You can either enter the full domain : <"www.mydomain.com">, or partially : <"mydomain.org"> / <"www.mydomain"> or even just the domain name itself : <"domain">.
+
 ## Dependencies
 
 First, install the requirements with :
--- src/ransomware.py
+++ src/ransomware.py
@@ -10,7 +10,8 @@ class RANSOsint:
         self.domains = {} # List of overseen domains
         self.api_urls = {
             "RansomWareLive" : "https://api.ransomware.live/v2/recentvictims", 
-            "RansomLook" : "https://www.ransomlook.io/api/recent"
+            "RansomLook" : "https://www.ransomlook.io/api/recent",
+            "Ransomfeed" : "https://api.ransomfeed.it/offset/100"
             } # API urls used to detect ransomware attacks
         self.json_response = {} # Response body from each API
         if len(sys.argv) != 5:
@@ -26,20 +27,20 @@ class RANSOsint:
     def get_responses(self): # Used to get all json body from APIs
         for key in self.api_urls:
             try:
-                response = requests.get(self.api_urls[key], timeout=5)
+                response = requests.get(self.api_urls[key], timeout=10)
                 
                 if response.status_code == 200:
                     self.json_response[key] = response.json()
                 else:
                     print('Error:', response.status_code)
             except requests.exceptions.Timeout as e:
-            
                 print('Error:', e)    
     
     def get_ransom_from_api(self, apikey: str): # Check if a given domain is present in a response body from a certain api
         domain_name = None
         url = None
         group = None
+        
         if apikey == "RansomWareLive":
             domain_name = "domain"
             url = "claim_url"
@@ -48,13 +49,21 @@ class RANSOsint:
             domain_name = "post_title"
             url = "link"
             group = "group_name"
+        elif apikey == "Ransomfeed":
+            domain_name = "victim"
+            group = "gang"
         
         for domain in self.domains["domains"]:    
             for victims in self.json_response[apikey]:
-                if domain in victims[domain_name]:
-                    claim_url = victims[url]
+                if domain.lower() in victims[domain_name].lower():
+                    if url == None:
+                        claim_url = f"No url found for this attack by api {apikey}"
+                    else :
+                        claim_url = victims[url]
+                    if victims[group] == None:
+                        attacker_group = "No group found for this attack"
                     attacker_group = "attacker group : " + victims[group]
-                    self.has_been_ransomwared[domain] = claim_url + " by " + attacker_group
+                    self.has_been_ransomwared[domain] = claim_url + ", by " + attacker_group
 
     def get_ransom_info(self): # Check if ay of the domain in the entry file <domains.json> has been ransmowared
         for apikey in self.json_response:
@@ -84,7 +93,6 @@ class RANSOsint:
             self.alert_sender.send_mail(self.receiver_mail, "Ransomware", all_domains, informations)
         elif length == 0:
             print("All given domains not found in ransomware lists, no mail sent.")
-            #exit(1)
         else:
             print("Something went wrong... Exiting.")
             exit(1)
@@ -93,4 +101,4 @@ if __name__ == "__main__":
     a = RANSOsint()
     while True:
         a.send_ransom_info()
-        time.sleep(5184000) #Execute bot every day (1d = 24 * 60 * 3600 seconds = 5184000 seconds)
+        time.sleep(86400) #Execute bot every day (1d = 24 * 3600 seconds = 86400 seconds)
